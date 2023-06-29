36d07f2cc0d2936c10e564e29b02e7536ee4c1ff
liangweiguo <2815171417@qq.com>
2023-06-29 23:32:16 +0800

    兼容

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const mysql = require('mysql');
 const db = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
-    password: "123456",
+    // password: "123456", // 公司
+    password: "root@123456", //自己
     port: 3306,
     database: "userdb",
     timezone: '08:00'
